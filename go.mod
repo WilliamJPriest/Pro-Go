7a7c5785e211f1bd0f0151ca59e03f98bd82e6e9
williamjpriest <liuliu6wp@gmail.com>
2023-10-23 10:47:14 -0400

    hashed password

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/williamjPriest/HTMXGO
 
 go 1.21.2
+
+require golang.org/x/crypto v0.14.0 // indirect
--- main.go
+++ main.go
@@ -3,23 +3,63 @@ package main
 import(
 	"net/http"
 	"html/template"
-	// "fmt"
-	"io"
+	"fmt"
+	// "io"
 	"log"
 	// "github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
+type userData struct{
+	username string
+	password string
+}
+
+var MPassword []byte
+
 func main(){
-	heyHandler := func(w http.ResponseWriter, req *http.Request){
+	loginPageHandler := func(w http.ResponseWriter, req *http.Request){
 		t := template.Must(template.ParseFiles("index.html"))
 		t.Execute(w, nil)
+	}	
+
+	registerPageHandler := func(w http.ResponseWriter, req *http.Request){
+		t := template.Must(template.ParseFiles("register.html"))
+		t.Execute(w, nil)
+
 	}
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request){
-		io.WriteString(w, "<h1>HI</h1>")
+	loginHandler := func(w http.ResponseWriter, req *http.Request){
+		username := req.PostFormValue("username")
+		password := req.PostFormValue("password")
+		fmt.Println(username)
+		err := bcrypt.CompareHashAndPassword([]byte(MPassword) , []byte(password))
+		fmt.Println(err == nil)
+		// if err != nil{
+		// 	log.Fatalf("didn't match: %s", err)
+		// }
+		
+		
+
 	}
+	registerHandler := func(w http.ResponseWriter, req *http.Request){
+		username := req.PostFormValue("username")
+		password := req.PostFormValue("password")
+		bcrypt,err := bcrypt.GenerateFromPassword([]byte(password), 5  )
+		if err != nil{
+			log.Fatalf("failed to hash: %s", err)
+		}
+		fmt.Println(username)
+		MPassword=bcrypt
+
+	}
+
+
 	
-	http.HandleFunc("/hey",heyHandler)
-	http.HandleFunc("/btn", helloHandler )
+	http.HandleFunc("/loginPage",loginPageHandler)
+	http.HandleFunc("/registerPage", registerPageHandler )	
+	http.HandleFunc("/login", loginHandler )
+	http.HandleFunc("/register", registerHandler )	
+
 	log.Fatal(http.ListenAndServe(":8000",nil))
 }
