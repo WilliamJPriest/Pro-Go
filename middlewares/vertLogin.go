0d4d36548914be0d4faefcb8055301e8a85f1b94
williamjpriest <liuliu6wp@gmail.com>
2023-11-02 06:33:07 -0400

    check user

--- middlewares/vertLogin.go
+++ middlewares/vertLogin.go
@@ -1,9 +1,34 @@
 package middlewares
 
-import "net/http"
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func VertifyLogin(endpointHandler func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file: %w", err)
+		 
+		}
+		DBlink := os.Getenv("DB_LINK")
+		dsn := DBlink
+		db, err := sql.Open("postgres", dsn)
+		if err != nil {
+			log.Fatal("Failed to execute query: %w" ,err)
+		}
+
+		rows, err := db.Query("SELECT * FROM Users WHERE username = $1, password = $2", username, email)
+		if err != nil {
+			log.Fatal("Error executing SQL query: %w", err)
+		}
+		defer rows.Close()
 
 	})
 }
