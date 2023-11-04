54571c7d42334401c72c7f46fa82f56191ac0c95
williamjpriest <liuliu6wp@gmail.com>
2023-11-04 08:36:00 -0400

    new bookmark database

--- database/createDB.go
+++ database/createDB.go
@@ -27,4 +27,10 @@ func Create(){
 		log.Fatal("failed to execute query", err)
 	}
 
+	_, err = db.Exec("CREATE TABLE if not exists Bookmarks (BookmarkID SERIAL PRIMARY KEY, username char(100),author TEXT, title TEXT, Description TEXT, Url TEXT, UrltoImage Text, Content TEXT);")
+	if err != nil {
+		log.Fatal("failed to execute query", err)
+	}
+
+
 }
