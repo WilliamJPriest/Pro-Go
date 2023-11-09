c3d83f97b8be73b7cacb4735c81702d28606de22
williamjpriest <liuliu6wp@gmail.com>
2023-11-09 11:30:44 -0500

    test check bookmark func

--- database/handleBookmarks.go
+++ database/handleBookmarks.go
@@ -1,5 +1,27 @@
 package database
 
-func AddBookMarks(){
-	
+import (
+	"fmt"
+)
+
+func AddBookMarks() {
+
+}
+
+func CheckBookMarks(title string) bool {
+
+	db,err := ConnectToDB()
+	if err != nil{
+		fmt.Println("%w",err)
+		return false
+	}
+	fmt.Println(title)
+	row := db.QueryRow("SELECT title FROM Bookmarks WHERE title = $1", title)
+    
+    if err := row.Scan(); err != nil {
+        fmt.Println("%w", err)
+        return false
+    }
+
+	return true
 }
--- middlewares/vertiBookmarks.go
+++ middlewares/vertiBookmarks.go
@@ -2,10 +2,13 @@ package middlewares
 
 import (
 	"net/http"
+	"github.com/williamjPriest/HTMXGO/database"
 )
 
 func VerifyBookmarks(endpointHandler func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		title := req.PostFormValue("Title")
+		database.CheckBookMarks(title)
 		endpointHandler(w, req)
 	})
 }
