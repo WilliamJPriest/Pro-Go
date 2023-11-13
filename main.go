6232f9a74f8c66cd1f00f28448889045cc7134b0
williamjpriest <liuliu6wp@gmail.com>
2023-11-13 06:07:22 -0500

    extended fwt

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func main(){
 
 
 		token := jwt.New(jwt.SigningMethodHS256)
-		expiration := time.Now().Add(10 * time.Minute)
+		expiration := time.Now().Add(60 * time.Minute)
 		claims := token.Claims.(jwt.MapClaims)
 		claims["exp"] = expiration.Unix()
 		claims["authorized"] = true
