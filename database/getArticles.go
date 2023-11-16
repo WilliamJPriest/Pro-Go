efd9bb40196267e38ad1626c4290e6a600a13abf
williamjpriest <liuliu6wp@gmail.com>
2023-11-16 11:10:54 -0500

    getarticle func basic

--- database/getArticles.go
+++ database/getArticles.go
@@ -1,5 +1,20 @@
 package database
 
-func GetArticles(){
-	
+import (
+	"fmt"
+	"log"
+)
+
+func GetArticles() (string, error) {
+	db, err := ConnectToDB()
+	if err != nil {
+		log.Fatal("%w", err)
+	}
+
+	defer db.Close()
+	_, err = db.Query("Select * from Articles")
+	if err != nil{
+		return "yo", fmt.Errorf("couldn't find in articles table: %w", err)
+	}
+	return "yo", nil
 }
--- main.go
+++ main.go
@@ -21,9 +21,13 @@ import (
 
 
 func main(){
-	
-	err := godotenv.Load()
-	if err != nil {
+	res, err := database.GetArticles()
+	if err != nil{
+		fmt.Println(err)
+	}
+	println(res)
+	err2 := godotenv.Load()
+	if err2 != nil {
 	  log.Fatal("Error loading .env file")
 	}
   
