7fbee79ed4264edbac1672dcc642eb455e993fa9
williamjpriest <liuliu6wp@gmail.com>
2023-11-30 05:46:16 -0500

    fixed bookmark glitch

--- middlewares/vertJWT.go
+++ middlewares/vertJWT.go
@@ -12,10 +12,6 @@ import (
 
 func VerifyJWT(endpointHandler func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		err := godotenv.Load()
-		if err != nil {
-			return nil, fmt.Errorf("Error loading .env file: %w", err)
-		}
 		secretCode := os.Getenv("SECRET_CODE")
 		var SecretKey = []byte(secretCode)
 		cookie, err := req.Cookie("token")
