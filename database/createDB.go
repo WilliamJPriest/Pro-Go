2d61c29a360598756f582c87ac6e5384306dfa17
williamjpriest <liuliu6wp@gmail.com>
2023-12-16 18:55:44 -0500

    register-email

--- database/createDB.go
+++ database/createDB.go
@@ -13,10 +13,15 @@ func Create() error{
 		log.Fatal("%w", err)
 	}
 
-	_, err = db.Exec("CREATE TABLE if not exists Users (UserID SERIAL PRIMARY KEY, username char(100),password char(100));")
+	_, err = db.Exec("CREATE TABLE if not exists Users (UserID SERIAL PRIMARY KEY, username char(100),password char(100),email char(100));")
 	if err != nil {
 		log.Fatal("failed to execute Users query", err)
 	}
+
+	// _, err = db.Exec("ALTER TABLE Users ADD COLUMN ;")
+	// if err != nil {
+	// 	log.Fatal("failed to execute alter query", err)
+	// }
 
 	_, err = db.Exec("CREATE TABLE if not exists Bookmarks (BookmarkID SERIAL PRIMARY KEY, username char(100),author tEXT, title TEXT, description TEXT, Url TEXT, urlToImage Text, content TEXT);")
 	if err != nil {
