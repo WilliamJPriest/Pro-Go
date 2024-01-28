dc13470475424428ce87a0e7e4f20084e9d257be
williamjpriest <liuliu6wp@gmail.com>
2024-01-28 04:58:49 -0500

    register-test

--- register_test.go
+++ register_test.go
@@ -3,27 +3,37 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/williamjPriest/HTMXGO/routes"
 )
 
-func TestRegisterHandler(t *testing.T) {
-	req, err := http.NewRequest("GET", "/register", nil)
+func TestRegisterController(t *testing.T) {
+	form := strings.NewReader("username=testuser&password=testpassword")
+
+
+	req, err := http.NewRequest("POST", "/register", form)
 	if err != nil {
 		t.Fatal(err)
 	}
+
 
 	recorder := httptest.NewRecorder()
 
 	routes.RegisterHandler(recorder, req)
 
+
+
 	if status := recorder.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
-	expectedContentType := "text/html; charset=utf-8"
-	if contentType := recorder.Header().Get("Content-Type"); contentType != expectedContentType {
-		t.Errorf("handler returned wrong Content-Type: got %v want %v", contentType, expectedContentType)
-	}
+}
+
+
+type MockDatabase struct{}
+
+func (m *MockDatabase) AddUser(username string, password []byte) error {
+	return nil
 }
